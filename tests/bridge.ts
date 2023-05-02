098c17d635dd08dd2ec0a883f1ac1bf817c906e5
Daniel Roe <daniel@roe.dev>
2023-05-02 22:16:49 +0100

    feat: add bridge test suite

---
+++ tests/bridge.ts
@@ -0,0 +1,19 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'nuxt/bridge',
+		build: ['dev:prepare', 'build'],
+		test: [
+			'test:fixtures',
+			'test:fixtures:dev',
+			'test:fixtures:webpack',
+			'test:fixtures:webpack:dev',
+		],
+		overrides: {
+			nuxt: '^2.16.3',
+		},
+	})
+}
