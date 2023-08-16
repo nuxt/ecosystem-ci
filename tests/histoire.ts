e134553f7c19976e1459c08510c36d1c602d9d94
Daniel Roe <daniel@roe.dev>
2023-08-16 13:20:15 +0200

    fix: update histoire commands

--- tests/histoire.ts
+++ tests/histoire.ts
@@ -5,8 +5,8 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'histoire-dev/histoire',
-		build: ['build', 'pnpm --filter histoire-example-nuxt3 story:build'],
-		test: ['pnpm --filter histoire-example-nuxt3 ci'],
+		build: ['build', 'pnpm --filter histoire-example-nuxt3 run story:build'],
+		test: ['pnpm --filter histoire-example-nuxt3 run ci'],
 		overrides: {
 			vue: false,
 			'@vue/compiler-sfc': false,
