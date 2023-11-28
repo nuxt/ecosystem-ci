86a062d12dc0c7cac2cc99a5d15a8bf68d1ce1ee
Daniel Roe <daniel@roe.dev>
2023-11-28 20:55:05 +0000

    fix: use vite 5 branch temporarily for vite-pwa

--- tests/vite-pwa.ts
+++ tests/vite-pwa.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'vite-pwa/nuxt',
-		branch: 'main',
+		branch: 'userquin/nuxt-ecosystem-ci-vite-5',
 		beforeTest: 'pnpm playwright install chromium',
 		build: ['dev:prepare', 'prepack'],
 		test: 'test',
