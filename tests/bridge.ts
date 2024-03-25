7256bc284bb8612b24e25c1cca8e5caab0162e03
Daniel Roe <daniel@roe.dev>
2024-03-25 09:25:41 +0000

    fix: install chromium before testing bridge

--- tests/bridge.ts
+++ tests/bridge.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'nuxt/bridge',
-		build: ['dev:prepare', 'build'],
+		build: ['pnpm playwright-core install chromium', 'dev:prepare', 'build'],
 		test: [
 			'test:fixtures',
 			'test:fixtures:dev',
