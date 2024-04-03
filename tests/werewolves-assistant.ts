1755f427a14e127072e67b287b3f13b750762761
Antoine Zanardi <antoine.zanardi@epitech.eu>
2024-04-03 10:48:35 +0200

    test: add `werewolves-assistant-web` (#8)

---
+++ tests/werewolves-assistant.ts
@@ -0,0 +1,16 @@
+import { runInRepo } from '../utils.ts'
+import type { RunOptions } from '../types.ts'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'antoinezanardi/werewolves-assistant-web-next',
+		build: ['build'],
+		test: [
+			'test:unit',
+			'docker:sandbox-api:start',
+			'test:cucumber:prepare',
+			'test:cucumber',
+		],
+	})
+}
