e31227047eadee2671691ccaab1a59ded20c0608
Daniel Roe <daniel@roe.dev>
2024-06-14 13:30:38 +0100

    feat: add `nuxt/scripts` to ecosystem

---
+++ tests/scripts.ts
@@ -0,0 +1,15 @@
+import { runInRepo } from '../utils.ts'
+import type { RunOptions } from '../types.js'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'nuxt/scripts',
+		beforeTest: 'pnpm playwright-core install chromium',
+		build: ['dev:prepare'],
+		test: ['test', 'build', 'test:types'],
+		overrides: {
+			esbuild: 'latest',
+		},
+	})
+}
