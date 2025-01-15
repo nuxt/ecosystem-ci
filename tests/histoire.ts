85096f2ac97b3d7b21f1996e97d94db98047f7ea
Daniel Roe <daniel@roe.dev>
2025-01-15 15:13:21 +0000

    fix(histoire): update overrides for `histoire`

--- tests/histoire.ts
+++ tests/histoire.ts
@@ -9,17 +9,8 @@ export async function test(options: RunOptions) {
     test: ['pnpm --filter histoire-example-nuxt3 run ci'],
     overrides: {
       'rollup': 'latest',
-      'vue': false,
-      '@vue/compiler-sfc': false,
-      '@vue/compiler-ssr': false,
-      '@vue/runtime-dom': false,
-      '@vue/server-renderer': false,
-      '@vue/compiler-core': false,
-      '@vue/reactivity': false,
-      '@vue/shared': false,
-      '@vue/compiler-dom': false,
-      '@vue/reactivity-transform': false,
-      '@vue/runtime-core': false,
+      '@nuxtjs/tailwindcss': 'latest',
+      'jiti': 'latest',
     },
   })
 }
