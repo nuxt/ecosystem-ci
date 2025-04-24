f68bddd5ef8036c7ee2cff46a47a4d0a6b8f9cb7
Antoine ZANARDI <antoine.zanardi.partner@decathlon.com>
2025-04-24 16:23:04 +0200

    feat: add antoine-zanardi-portfolio integration and test configuration

---
+++ tests/antoine-zanardi-portfolio.ts
@@ -0,0 +1,15 @@
+import type { RunOptions } from '../types.ts'
+import { runInRepo } from '../utils.ts'
+
+export async function test(options: RunOptions) {
+  await runInRepo({
+    ...options,
+    repo: 'antoinezanardi/antoinezanardi.fr',
+    build: ['build'],
+    test: [
+      'test:unit:cov',
+			'test:cucumber:prepare',
+			'test:cucumber',
+    ],
+  })
+}
