2583c7513b7ea19cd4f546f17529934a5547a586
Daniel Roe <daniel@roe.dev>
2025-06-11 12:12:48 +0200

    fix: update docus test commands

--- tests/docus.ts
+++ tests/docus.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
   await runInRepo({
     ...options,
     repo: 'nuxt-themes/docus',
-    build: ['prepare'],
-    test: ['generate'],
+    build: ['dev:prepare'],
+    test: ['docs:build'],
   })
 }
