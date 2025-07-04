8d38efd719a6180981cbff51132282875af605fa
Daniel Roe <daniel@roe.dev>
2025-07-04 12:59:45 +0100

    chore: add override for tailwindcss + vitest

--- tests/tailwindcss.ts
+++ tests/tailwindcss.ts
@@ -4,6 +4,9 @@ import { runInRepo } from '../utils.ts'
 export async function test(options: RunOptions) {
   await runInRepo({
     ...options,
+    overrides: {
+      vitest: '^3.2.0',
+    },
     repo: 'nuxt-modules/tailwindcss',
     branch: 'main',
     build: ['dev:prepare', 'build'],
