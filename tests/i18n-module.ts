95175b5ee737e3aa20aedd9acbc00419dedbf0ab
Daniel Roe <daniel@roe.dev>
2025-12-19 08:36:37 +0000

    fix(i18n-module): pin vue-router to latest

--- tests/i18n-module.ts
+++ tests/i18n-module.ts
@@ -5,6 +5,9 @@ export async function test(options: RunOptions) {
   await runInRepo({
     ...options,
     shallow: false,
+    overrides: {
+      'vue-router': 'latest',
+    },
     branch: 'main',
     beforeTest: 'pnpm playwright-core install chromium',
     repo: 'nuxt-modules/i18n',
