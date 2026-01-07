08477e001a1fdc176fd296157cc8871a85c24a32
Daniel Roe <daniel@roe.dev>
2026-01-07 10:30:17 +0000

    fix(test-utils): do not override nitro/nitropack/h3

--- tests/test-utils.ts
+++ tests/test-utils.ts
@@ -4,6 +4,11 @@ import { runInRepo } from '../utils.ts'
 export async function test(options: RunOptions) {
   await runInRepo({
     ...options,
+    overrides: {
+      nitropack: false,
+      h3: false,
+      nitro: false,
+    },
     repo: 'nuxt/test-utils',
     build: ['pnpm playwright-core install chromium', 'dev:prepare', 'prepack'],
     test: ['test:examples'],
