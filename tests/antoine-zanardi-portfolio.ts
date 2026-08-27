3ca9239cc7bf1448308f37fd8b156c8bbd3aee36
Daniel Roe <daniel@roe.dev>
2026-08-27 23:26:54 +0000

    fix(antoine-zanardi-portfolio): override `@nuxt/image` to latest

--- tests/antoine-zanardi-portfolio.ts
+++ tests/antoine-zanardi-portfolio.ts
@@ -6,6 +6,9 @@ export async function test(options: RunOptions) {
     ...options,
     repo: 'antoinezanardi/antoinezanardi.fr',
     branch: 'master',
+    overrides: {
+      '@nuxt/image': 'latest',
+    },
     build: ['build'],
     test: [
       'test:unit:cov',
